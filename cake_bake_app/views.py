c5751e855d3f77ad920fb3ee4f1bf226477db802
Valery Efremov <valeriy131100@gmail.com>
2022-04-26 19:26:16 +0300

    Add templates and static

--- cake_bake_app/views.py
+++ cake_bake_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def profile(request):
+    return render(request, 'lk.html')
+
+
+def profile_order(request):
+    return render(request, 'lk-order.html')
