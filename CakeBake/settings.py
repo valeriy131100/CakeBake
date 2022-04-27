1617a861f98829a09f9fcb768e292e32858f215f
Alexander Ten <orionapplepie@gmail.com>
2022-04-27 14:55:30 +1000

    Add custom user model.

--- CakeBake/settings.py
+++ CakeBake/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'cake_bake_app',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,5 @@ STATICFILES_DIRS = ['static']
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'cake_bake_app.User'
