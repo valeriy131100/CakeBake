19a65eda66723e20e3f8c72046138b6504564fdb
Valery Efremov <valeriy131100@gmail.com>
2022-04-28 18:10:13 +0300

    Return payment and some other

--- cake_bake_app/views.py
+++ cake_bake_app/views.py
@@ -1,7 +1,16 @@
+import json
+import threading
+import time
+import uuid
+
+from django.conf import settings
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.decorators.http import require_POST
+from yookassa import Payment, Configuration
 
 from .models import LevelsQuantity, CakeForm, Topping, Berry, Decor, Order
-
 
 temped_orders = {}
 
@@ -36,22 +45,71 @@ def index(request):
         'decors': prepare_components_list(decors),
     }
 
-    user_orders = Order.objects.filter(user=request.user).prefetch_related('cake')
-    order_quantity = {
-        'order_quantity': user_orders.count()
-    }
-
     return render(
         request, 'index.html',
         context={
             'components': components,
-            'order_quantity': order_quantity,
             }
         )
 
 
 def profile(request):
     return render(request, 'lk.html')
+
+
+def check_payment_until_confirm(payment_id, subscription_uuid):
+    while True:
+        payment = Payment.find_one(payment_id)
+        if payment.status == "canceled":
+            temped_orders.pop(subscription_uuid)
+            return
+        if payment.status == "succeeded":
+            order = temped_orders[subscription_uuid]
+            # save order here
+
+            print('Order finished!')
+            print(order)
+
+            return
+
+        time.sleep(5)
+
+
+@require_POST
+def payment(request):
+    order_description = json.loads(request.body)
+    order_uuid = uuid.uuid4()
+
+    Configuration.account_id = settings.YOOKASSA_ACCOUNT_ID
+    Configuration.secret_key = settings.YOOKASSA_SECRET_KEY
+
+    yoo_payment = Payment.create({
+        "amount": {
+            "value": order_description['Cost'],
+            "currency": "RUB"
+        },
+        "confirmation": {
+            "type": "redirect",
+            "return_url": request.build_absolute_uri(reverse(profile))
+        },
+        "capture": True,
+        "description": None
+    })
+
+    temped_orders[order_uuid] = order_description
+
+    threading.Thread(
+        target=check_payment_until_confirm,
+        args=[yoo_payment.id, order_uuid],
+        daemon=True
+    ).start()
+
+    return JsonResponse(
+        {
+            'status': 'success',
+            'redirect': yoo_payment.confirmation.confirmation_url
+        }
+    )
 
 
 def order_list(request):
