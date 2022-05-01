7d3e52ae4ce49aaf9691f1504cd5a2c94009dd93
Alexander Ten <orionapplepie@gmail.com>
2022-05-01 20:49:15 +1000

    Add favicon.

--- CakeBake/urls.py
+++ CakeBake/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 from cake_bake_app import views
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'))),
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('login/', views.login_or_register),
