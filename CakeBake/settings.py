63ddf64425080f19804726726d5a6af2ae79940e
Valery Efremov <valeriy131100@gmail.com>
2022-05-02 09:59:05 +0300

    Add database url

--- CakeBake/settings.py
+++ CakeBake/settings.py
@@ -71,12 +71,7 @@ WSGI_APPLICATION = 'CakeBake.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {"default": env.dj_db_url("DATABASE_URL", default="sqlite:///db.sqlite3")}
 
 
 # Password validation
